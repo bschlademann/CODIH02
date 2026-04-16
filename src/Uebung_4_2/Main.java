5c3fd8c3e16841a330be105c66a5340271546093
Benni-beep <krikelkr4kel@gmail.com>
2026-04-16 15:23:47 +0200

    Übung 4.2

---
+++ src/Uebung_4_2/Main.java
@@ -0,0 +1,24 @@
+package Uebung_4_2;
+
+/**
+ * Erstellen Sie ein Programm zur Berechnung der Fakultät einer positiven ganzen Zahl n,
+ * diese ist definiert durch f (0) = 1 und f (n) = 1 * 2 * …* n für n > 0.
+ * Erstellen Sie eine innere statische Klasse OverflowException,
+ * die von der Klasse Exception abgeleitet wird.
+ * Die Klasse soll einen Konstruktor besitzen, dieser hat einen Parameter vom Typ String,
+ * der den Grund für die Exception angibt.
+ * Implementieren Sie dann die Methode factorial zur Berechnung der Fakultät.
+ * Die Methode hat einen Parameter n vom Typ int.
+ * Berechnen Sie das Ergebnis in einer Variable vom Typ long
+ * und überprüfen Sie während der Berechnung, ob der Wert größer als der maximale Wert einer Integer-Variablen ist.
+ * Dieser ist durch die Konstante Integer.MAX_VALUE gegeben. Ist dies der Fall,
+ * dann soll eine Exception der Klasse OverflowException ausgeworfen werden.
+ * Implementieren Sie eine main-Methode, die einen Wert n von der Konsole einliest
+ * und die Fakultät von n mit der Methode aus factorial berechnet.
+ * Die main-Methode soll dabei die Exceptions vom Typ OverflowException auffangen.
+ */
+public class Main {
+    void main () {
+
+    }
+}
