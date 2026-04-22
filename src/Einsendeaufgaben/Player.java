fbd399714ebb1e50f71b4636bfc58230d5716352
Benni-beep <krikelkr4kel@gmail.com>
2026-04-22 15:16:48 +0200

    feat: add class Player

---
+++ src/Einsendeaufgaben/Player.java
@@ -0,0 +1,27 @@
+package Einsendeaufgaben;
+
+/**
+ * Die Klasse Player benötigt zwei private Instanzvariablen,
+ * einen String für den Namen
+ * und eine Variable vom Typ Token für die Spielsteine der Spielerin bzw. des Spielers.
+ * Ergänzen Sie weiterhin einen Konstruktor mit zwei Parametern vom Typ String und Token,
+ * der die Instanzvariablen damit initialisiert,
+ * sowie zwei Getter-Methoden für die beiden Variablen.
+ */
+public class Player {
+    private String name;
+    private Token token;
+
+    public Player(String name, Token token) {
+        this.name = name;
+        this.token = token;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Token getToken() {
+        return token;
+    }
+}
