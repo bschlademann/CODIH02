4f382c2af3c2b79b30c9bc5e1e782248febd603c
Benni-beep <krikelkr4kel@gmail.com>
2026-04-22 15:17:25 +0200

    feat: add custom Exception GameException

---
+++ src/Einsendeaufgaben/GameException.java
@@ -0,0 +1,12 @@
+package Einsendeaufgaben;
+
+/**
+ * Die Klasse GameException wird von der API-Klasse Exception abgeleitet
+ * und besitzt einen Konstruktor vom Typ String, der den Grund für die Ausnahme angibt.
+ * Rufen Sie mit dem Parameter den Konstruktor der Oberklasse auf.
+ */
+public class GameException extends Exception {
+    public GameException(String message) {
+        super(message);
+    }
+}
