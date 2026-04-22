85213151aff2dd1595764d793158ef90ca3b60e2
Benni-beep <krikelkr4kel@gmail.com>
2026-04-22 15:16:55 +0200

    feat: add class Token

---
+++ src/Einsendeaufgaben/Token.java
@@ -0,0 +1,21 @@
+package Einsendeaufgaben;
+
+/**
+ * Die Klasse Token benötigt eine private Instanzvariable vom Typ int für die Farbe des Spielsteins.
+ * Legen Sie zwei öffentliche, statische Konstanten YELLOW und RED für die Farben der Spielsteine an.
+ * Implementieren Sie einen Konstruktor mit einem Parameter zum Initialisieren der Instanzvariable
+ * und eine Getter-Methode, die den Wert der Instanzvariable zurückgibt.
+ */
+public class Token {
+    private int color;
+    public static final int RED = 1;
+    public static final int YELLOW = 2;
+
+    public Token(int color) {
+        this.color = color;
+    }
+
+    public int getColor() {
+        return color;
+    }
+}
