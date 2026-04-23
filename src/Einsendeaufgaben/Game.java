377f2ab12956759a9b3dc89679bd45f26fcb7952
Benni-beep <krikelkr4kel@gmail.com>
2026-04-23 14:21:44 +0200

    feat: add Game.java

---
+++ src/Einsendeaufgaben/Game.java
@@ -0,0 +1,18 @@
+package Einsendeaufgaben;
+
+public abstract class Game {
+    protected Player[] players;
+    protected Gameboard board;
+
+    public Game(String playerOne, String playerTwo) {
+        this.players = new Player[2];
+        players[0] = new Player(playerOne, new Token(1));
+        players[1] = new Player(playerTwo, new Token(2));
+    }
+
+    abstract void play();
+
+    abstract void printBoard() throws GameException;
+
+    abstract void printWinner(Player player);
+}
